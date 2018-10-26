cf77ab1b4b56022432c8f630172b626a52677852
ruslan <chippo78@mail.ru>
2018-10-26 20:48:38 +0300

    add inclusive and exclusive options

--- src/main/java/ru/javaschool/mobileoperator/domain/Option.java
+++ src/main/java/ru/javaschool/mobileoperator/domain/Option.java
@@ -29,6 +29,16 @@ public class Option extends AbstractPO {
     @Column(name = "connection_cost")
     private Integer connectionCost;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_id")
+    private Option optionWithReferences;
+
+    @OneToMany(mappedBy = "optionWithReferences")
+    private Set<Option> exclusiveOptions;
+
+    @OneToMany(mappedBy = "optionWithReferences")
+    private Set<Option> inclusiveOptions;
+
     public Set<TariffPlan> getTariffPlans() {
         return tariffPlans;
     }
@@ -67,5 +77,29 @@ public class Option extends AbstractPO {
 
     public void setConnectionCost(Integer connectionCost) {
         this.connectionCost = connectionCost;
+    }
+
+    public Option getOptionWithReferences() {
+        return optionWithReferences;
+    }
+
+    public void setOptionWithReferences(Option optionWithReferences) {
+        this.optionWithReferences = optionWithReferences;
+    }
+
+    public Set<Option> getExclusiveOptions() {
+        return exclusiveOptions;
+    }
+
+    public void setExclusiveOptions(Set<Option> exclusiveOptions) {
+        this.exclusiveOptions = exclusiveOptions;
+    }
+
+    public Set<Option> getInclusiveOptions() {
+        return inclusiveOptions;
+    }
+
+    public void setInclusiveOptions(Set<Option> inclusiveOptions) {
+        this.inclusiveOptions = inclusiveOptions;
     }
 }
